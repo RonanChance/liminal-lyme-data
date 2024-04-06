8e85ba9af5dea5167716afe51b5c28c6e0d205d8
RonanChance <chancesec@proton.me>
2024-04-06 18:08:58 -0400

    Remove constants, add to utilities

--- utilities.py
+++ utilities.py
@@ -1,9 +1,11 @@
 import json
+import fileinput
+import os
 
 def clean_text(text):
     return ''.join(char for char in text if 32 <= ord(char) <= 126)
 
-def gpt_prep():
+def prep_for_gpt():
     with open('mydatav2.json', 'r', encoding="utf-8") as json_file:
         data = json.load(json_file)
 
@@ -29,8 +31,25 @@ def gpt_prep():
                 print("FOUND ISSUE")
                 exit()
                 continue
+
+def get_subreddit_names():
+    directory_path = "./TheEye/repo/"
+    reading_filenames = sorted(list(set([f.split('_')[0] for f in os.listdir(directory_path) if os.path.isfile(os.path.join(directory_path, f))])))
+    print(reading_filenames)
+
+def get_number_of_comments():
+    total_lines = 0
+    directory_path = "./TheEye/extracted/"
+    reading_filenames = [directory_path+f for f in os.listdir(directory_path) if os.path.isfile(os.path.join(directory_path, f))]
+    
+    for filename in reading_filenames:
+        file = fileinput.input(filename)
+        for _ in file:
+            total_lines += 1
+
+    print(total_lines)
         
-def wikipedia_link_generation():
+def generate_wikipedia_links():
     illnesses = ['ANAPLASMOSIS', 'BABESIOSIS', 'BARTONELLOSIS', 'EHRLICHIOSIS', 'LYME DISEASE', 'ROCKY MOUNTAIN SPOTTED FEVER', 'TULAREMIA']
     medications = ['ACETAMINOPHEN', 'ACETAZOLAMIDE', 'ACYCLOVIR', 'ALPRAZOLAM', 'AMITRIPTYLINE', 'AMOXICILLIN', 'ARIPIPRAZOLE', 'AZITHROMYCIN', 'AZLOCILLIN', 'BOTULINUM TOXIN TYPE A', 'CEFTRIAXONE', 'CEFUROXIME', 'CELECOXIB', 'CEPHALEXIN', 'CETIRIZINE', 'CHOLESTYRAMINE', 'CIPROFLOXACIN', 'CITALOPRAM', 'CLARITHROMYCIN', 'CLINDAMYCIN', 'CLONAZEPAM', 'COLCHICINE', 'CROMOGLICIC ACID', 'CYCLOBENZAPRINE', 'DAPTOMYCIN', 'DEXAMETHASONE', 'DIAZEPAM', 'DICYCLOMINE', 'DIPHENHYDRAMINE', 'DISULFIRAM', 'DOXYCYCLINE', 'DULOXETINE', 'ESCITALOPRAM', 'ESOMEPRAZOLE', 'FENTANYL', 'FLUCONAZOLE', 'FLUOXETINE', 'FLUVOXAMINE', 'HYDROCODONE', 'HYDROCORTISONE', 'HYDROXYCHLOROQUINE', 'HYDROXYZINE', 'L-TRYPTOPHAN', 'LEVOFLOXACIN', 'LEVOTHYROXINE', 'LIOTHYRONINE', 'LORAZEPAM', 'MECLIZINE', 'MELOXICAM', 'METHOTREXATE', 'METHYLPHENIDATE', 'METHYLPREDNISOLONE', 'METRONIDAZOLE', 'MINOCYCLINE', 'MIRTAZAPINE', 'NALTREXONE', 'NAPROXEN', 'NIACIN', 'NORTRIPTYLINE', 'NYSTATIN', 'OMEPRAZOLE', 'ONDANSETRON', 'OXYCODONE', 'PANTOPRAZOLE', 'PAROXETINE', 'PENICILLIN G', 'PREGABALIN', 'PROMETHAZINE', 'RANITIDINE', 'RIFAMPIN', 'RIFAXIMIN', 'SERTRALINE', 'TETRACYCLINE', 'TINIDAZOLE', 'TOPIRAMATE', 'TRAZODONE', 'VALACICLOVIR', 'VANCOMYCIN', 'VENLAFAXINE', 'ZINC']
 
@@ -41,3 +60,10 @@ def wikipedia_link_generation():
 
     print(medication_dict)
     print(len(medications))
+
+
+if __name__ == '__main__':
+    get_subreddit_names()
+    # generate_wikipedia_links()
+    # get_number_of_comments()
+    # prep_for_gpt()
